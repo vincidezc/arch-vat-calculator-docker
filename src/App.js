23a7a1f3325bd1ebddfea4446d92e253d362e168
qatrainer <36920585+qatrainer@users.noreply.github.com>
2024-12-12 21:05:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className='header field'>
-      VAT CALCULATOR from Peter
+      ARCH VAT CALCULATOR from your Name
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
